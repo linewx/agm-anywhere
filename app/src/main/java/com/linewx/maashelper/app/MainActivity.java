a7f739b87e9b84b10573325e27faeda274eb6601
Lu Ganlin <ganlin.lu@hp.com>
2014-05-31 15:54:54 +0800

    init project

---
+++ app/src/main/java/com/linewx/maashelper/app/MainActivity.java
@@ -0,0 +1,60 @@
+package com.linewx.maashelper.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void login(View source) {
+        EditText location = (EditText) findViewById(R.id.location);
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+        EditText domain = (EditText) findViewById(R.id.domain);
+        EditText project = (EditText) findViewById(R.id.project);
+
+        Dialog alertDialog = new AlertDialog.Builder(this).
+                setTitle("login information").
+                setMessage(location.getText()).
+                setIcon(R.drawable.ic_launcher).
+                create();
+        alertDialog.show();
+
+
+    }
+
+}
